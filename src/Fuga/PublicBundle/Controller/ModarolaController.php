12b6fcdbdfc3bf959b516ca3c3c199b85f9c1f1a
Roman Alyakritskiy <rawork@yandex.ru>
2016-09-17 08:46:27 +0300

    fixes

--- src/Fuga/PublicBundle/Controller/ModarolaController.php
+++ src/Fuga/PublicBundle/Controller/ModarolaController.php
@@ -52,70 +52,57 @@ class ModarolaController extends PublicController
 
 	public function productsAction()
 	{
-		$response = new JsonResponse();
 		$this->get('log')->addError('Modarola products');
 
-		if ($this->get('request')->headers->has('X-Auth-Token')
-			&& MODAROLA_AUTH_TOKEN == $this->get('request')->headers->get('X-Auth-Token')) {
+		$url = "http://modarola.ru/api/products";
+		$response = \Httpful\Request::get($url)
+			->expectsJson()
+			->addHeader('X-Auth-Token', MODAROLA_AUTH_TOKEN)
+			->send();
 
-			$json = file_get_contents('php://input');
-			$this->get('log')->addError($json);
-			$data = json_decode($json, TRUE);
+		$articuls = array();
+		$products = $this->get('container')->getItems('catalog_product');
+		foreach ($products as $product) {
+			$articuls[$product['articul']] = $product['id'];
+		}
+		$sizes = $this->get('container')->getItems('catalog_sku');
 
-			$articuls = array();
-			$products = $this->get('container')->getItem('catalog_products');
-			foreach ($products as $product) {
-				$articuls[$product['id']] = $product['articul'];
+		foreach ($response->body as $variant) {
+			$size = $this->get('container')->getItem('catalog_sku', 'modarola_id='.$variant->variant_id);
+			if (!array_key_exists($variant->product_sku, $articuls)) {
+				continue;
 			}
-			$sizes = $this->get('container')->getItems('catalog_sku');
-
-			foreach ($data as $variant) {
-				$size = $this->get('container')->getItem('catalog_sku', 'modarola_id='.$variant[0]);
-				if (!array_key_exists($variant[2], $articuls)) {
-					continue;
-				}
-				if ($size) {
-					$this->get('container')->updateItem(
-						'catalog_sku',
-						array('qunanity' => $variant[8]),
-						array('id' => $size['id'])
-					);
-					unset($sizes[$size['id']]);
-				} else {
-					$this->get('container')->addItem(
-						'catalog_sku',
-						array(
-							'modarola_id' => $variant[0],
-							'product_id' => $articuls[$variant[2]],
-							'name' => $variant[7],
-							'quntity' => $variant[8],
-						)
-					);
-				}
-				if (count($sizes) > 0) {
-					$ids = array_keys($sizes);
-					foreach ($ids as $id) {
-						$this->get('container')->updateItem(
-							'catalog_sku',
-							array('publish' => 0),
-							array('id' => $id)
-						);
-					}
-
-				}
-
+			if ($size) {
+				unset($sizes[$size['id']]);
+			} else {
+				$this->get('container')->addItem(
+					'catalog_sku',
+					array(
+						'modarola_id' => $variant->variant_id,
+						'product_id' => $articuls[$variant->product_sku],
+						'size' => $variant->variant_size,
+						'publish' => $variant->product_enabled == 'Y' ? 1 : 0,
+					)
+				);
 			}
-
-			$response->setData(array(
-				'message' => 'ok',
-			));
-
-		} else {
-			$response->setData(array(
-				'message' => 'no',
-			));
 		}
 
+		if (count($sizes) > 0) {
+			$ids = array_keys($sizes);
+			foreach ($ids as $id) {
+				$this->get('container')->updateItem(
+					'catalog_sku',
+					array('publish' => 0),
+					array('id' => $id)
+				);
+			}
+
+		}
+
+		$response = new JsonResponse();
+		$response->setData(array(
+			'message' => 'ok',
+		));
 
 		return $response;
 	}
