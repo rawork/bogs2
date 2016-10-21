5fc02f6d1f0e0353e02bc9f063b91b2f4ba11543
Roman Alyakritskiy <rawork@yandex.ru>
2016-10-21 10:55:08 +0300

    fixes

--- bundles/app/js/modules/autocomplete.js
+++ bundles/app/js/modules/autocomplete.js
@@ -6,27 +6,38 @@ var updateTotals = require('./update-totals'),
 module.exports = (function() {
 
     var field = $('.select-region__field'),
-        regions;
+        regions,
+        lists = {'cities': [], 'regions': [], 'countries': []};
 
     // Get Regions
     $.getJSON('/api/basket/regions.json', function(data){
         regions = data;
+        lists.regions = data
+    });
+
+    $.getJSON('/api/basket/cities.json', function(data){
+        lists.cities = data
+    });
+
+    $.getJSON('/api/basket/countries.json', function(data){
+        lists.countries = data
     });
 
     // Search Filter Function
-    function filterRegion(arr, query, autocompleteList) {
+    function filterRegion(arr, query, autocompleteList, context) {
         var newQuery = query.toLowerCase(),
             newList = [];
         var newArr = arr.filter(function(item) {
             if ( newQuery === item.subject.toLowerCase() ) {
+                //console.log(item);
                 setRegion(field, autocompleteList, item.subject);
-                updateTotals(item.price);
+                updateTotals(item[context]);
                 return;
             }
             return item.subject.toLowerCase().indexOf(newQuery) >= 0 ;
         }); 
         for (var i = 0, max = newArr.length; i < max; i++) {
-            newList.push('<li class="autocomplete__item"><span class="autocomplete__item__name">' + newArr[i].subject + '</span> – <span class="autocomplete__item__price">' + newArr[i].price + '</span>' + '\xa0руб.</li>');
+            newList.push('<li class="autocomplete__item"><span class="autocomplete__item__name">' + newArr[i].subject + '</span> – <span class="autocomplete__item__price">' + newArr[i][context] + '</span>' + '\xa0руб.</li>');
         }
         autocompleteList.html( newList.join('') );
     }
@@ -34,6 +45,9 @@ module.exports = (function() {
     // Listen keyup
     var timer;
     field.on('keyup', function (e) {
+        var context = $(this).attr('data-context');
+        var source = $(this).attr('data-source');
+        //console.log(context, source);
         if( e.which <= 90 && e.which >= 48 || e.which == 8 ){
             var input = $(this);
             if (timer) clearTimeout(timer); // clear timer
@@ -41,7 +55,7 @@ module.exports = (function() {
                 var query = input.val(), // value of input
                     autocompleteList = input.siblings('.autocomplete'); // value of input
                 if (query !== '') {
-                    filterRegion(regions, query, autocompleteList);
+                    filterRegion(lists[source], query, autocompleteList, context);
                 } else {
                     autocompleteList.html('');
                     field.removeClass('selected');
