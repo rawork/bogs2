e5c3f0ef45b0bcfe9b014be1700cd093e8f0b7d9
Roman Alyakritskiy <rawork@yandex.ru>
2016-12-01 23:25:57 +0300

    fixes

--- src/Fuga/PublicBundle/Controller/ExportController.php
+++ src/Fuga/PublicBundle/Controller/ExportController.php
@@ -46,7 +46,7 @@ class ExportController extends PublicController
 	<offers>
 ';
 		foreach ($products as $product) {
-			$content .= '		<offer id="'.$product['id'].'" available="true" cbid="4.50">
+			$content .= '		<offer id="'.$product['id'].'" available="true" cbid="'.$product['cbid'].'">
 			<url>http://'.$_SERVER['SERVER_NAME'].'#/'.$product['id'].'</url>
 			<price>'.$product['price'].'</price>
 			<currencyId>RUR</currencyId>
@@ -76,7 +76,53 @@ class ExportController extends PublicController
 
 	public function googleAction()
 	{
+		$products = $this->get('container')->getItems('catalog_product', 'publish=1');
+		foreach ($products as &$product) {
+			$product['sku'] = $this->get('container')->getItems('catalog_sku', 'publish=1 AND (quantity > 0 OR quantity2 > 0) AND product_id='.$product['id']);
+		}
+		unset($product);
+		$date = date('Y-m-d');
+		$time = date('H:i:s');
 
+		$content = '<?xml version="1.0"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="http://base.google.com/ns/1.0">
+	<title>'.$this->shop.' - Online Store</title>
+	<link rel="self" href="'.$this->url.'"/>
+	<updated>'.$date.'T'.$time.'Z</updated> 
+		';
+
+		foreach ($products as $product) {
+			foreach ($product['sku'] as $sku) {
+				$content .= '	<entry>
+		<!-- The following attributes are always required -->
+		<g:id>'.$product['articul'].'-'.$sku['id'].'</g:id>
+		<g:title>'.$product['name'].' - Размер '.$sku['size'].' US</g:title>
+		<g:description>'.$product['description'].'</g:description>
+		<g:link>http://'.$_SERVER['SERVER_NAME'].'/#/'.$product['id'].'/'.$sku['id'].'</g:link>
+		<g:image_link>http://'.$_SERVER['SERVER_NAME'].$product['photo_value']['extra']['main']['path'].'</g:image_link>
+		<g:condition>new</g:condition>
+		<g:availability>'.($product['is_preorder'] == 1 ? 'preorder' : 'in stock').'</g:availability>	
+		<g:price>'.$product['price'].' RUR</g:price>
+		<g:google_product_category>187</g:google_product_category>
+		<g:brand>BOGS</g:brand>
+		<g:gender>'.$product['gender'].'</g:gender>
+		<g:age_group>'.$product['age_group'].'</g:age_group>
+		<g:size>'.$sku['size'].'</g:size>
+				
+		<g:item_group_id>'.$product['articul'].'</g:item_group_id>
+	</entry>
+				
+';
+			}
+		}
+
+		$content .= '</feed>';
+
+		$response = new Response();
+		$response->setContent($content);
+		$response->headers->set('Content-Type', 'xml');
+
+		return $response;
 	}
 
 } 
